aa50a9f08d97dc9c1003d67ab7506c25e4cf6cc0
Gastygas <gastonrodrigogonzalez55@gmail.com>
2024-11-22 01:14:08 -0300

    images

--- front/src/components/Section-2/Section-2.tsx
+++ front/src/components/Section-2/Section-2.tsx
@@ -4,7 +4,10 @@ import CreateCarousel from "../CarouselPhotos/CarouselPhotos"
 const Section2 = () => {
     const photos: string[] =[
         "https://res.cloudinary.com/dbmfju6mu/image/upload/v1732221553/d_rn6vip.jpg",
-        "https://res.cloudinary.com/dbmfju6mu/image/upload/v1732221442/b_vqrjvf.png"
+        "https://res.cloudinary.com/dbmfju6mu/image/upload/v1732221442/b_vqrjvf.png",
+        "https://res.cloudinary.com/dbmfju6mu/image/upload/v1732248331/c_jqnqsu.png",
+        "https://res.cloudinary.com/dbmfju6mu/image/upload/v1732248602/e_ippd9u.jpg",
+        "https://res.cloudinary.com/dbmfju6mu/image/upload/v1732248779/Screenshot_2024-11-22_011154_vhk7hm.png",
     ]
     return(
         <div className={styles.container}>
