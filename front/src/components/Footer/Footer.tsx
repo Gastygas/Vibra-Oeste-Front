2b61b254197509ab40b8890150cf41b6ece4c01f
Gastygas <gastonrodrigogonzalez55@gmail.com>
2024-12-23 14:40:38 -0300

    error images

--- front/src/components/Footer/Footer.tsx
+++ front/src/components/Footer/Footer.tsx
@@ -3,14 +3,16 @@ import Image from "next/image"
 import styles from "./Footer.module.css"
 import logo from "../../assets/logo.png"
 const Footer = () => {
-    return(
+  const linkedIn = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/1024px-LinkedIn_icon.svg.png"
+  const gitHub = "https://images.seeklogo.com/logo-png/30/2/github-logo-png_seeklogo-304612.png?v=638664844220000000"
+  return(
         <footer className={styles.footer}>
     <div className={`container ${styles.footerSpace}`}>
     <Link href="/"><Image src={logo} alt="logo" width={100} height={60} className={styles.logo}/></Link>
       <p className={styles.text}>Creado por <Link href="/about">Gaston Gonzalez </Link>🍃</p>
       <div className={styles.socialMedia}>
-        <Link href="/"><Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/1024px-LinkedIn_icon.svg.png" alt="LinkedIn Logo"/></Link>
-        <Link href="/"><Image src="https://images.seeklogo.com/logo-png/30/2/github-logo-png_seeklogo-304612.png?v=638664844220000000" alt="GitHub Logo"/></Link>
+        <Link href="/"><Image src={linkedIn} alt="LinkedIn Logo" width={100} height={60} /></Link>
+        <Link href="/"><Image src={gitHub} alt="GitHub Logo" width={1} height={1}/></Link>
       </div>
       <p className={styles.copyright}>&copy; no copyright</p>
     </div>
