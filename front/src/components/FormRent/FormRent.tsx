6de3fa96b955696bd06cbeaa9a0ac943207d9bc5
Gastygas <gastonrodrigogonzalez55@gmail.com>
2025-01-06 18:05:41 -0300

    new form

--- front/src/components/FormRent/FormRent.tsx
+++ front/src/components/FormRent/FormRent.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import styles from "./FormRent.module.css";
-import { validateAddress, validateName, validatePhone } from "@/helpers/validation";
+import { validateAddress, validateCity, validateName, validateNumberAddress, validatePhone, validateStartHour } from "@/helpers/validation";
 import { formService } from "@/services/form.service";
 import { Loader } from '@googlemaps/js-api-loader'
 
@@ -9,7 +9,11 @@ export interface IInitialData{
   name:string;
   surname:string;
   telephone:string;
+  city:string;
+  numberAddress:string;
   address:string;
+  startHour:string;
+  endHour?:string;
   speakers?:string;
   console?:string;
   microphones?:string;
@@ -18,86 +22,88 @@ export interface IInitialData{
 }
 
 const Form = () => {
-  const initialData: IInitialData = {name: "", surname: "",telephone: "", address:"",microphones:"0",console:"0",speakers:"0",mapLocation:{lat: 24, lng: 42}, addressMaps: ""}
-  const initialDirty = {name: false, surname:false , telephone:false, address:false}
+  const initialData: IInitialData = {name: "", surname: "",telephone: "", address:"",city:"",numberAddress:"",startHour:"",microphones:"0",console:"0",speakers:"0",mapLocation:{lat: 24, lng: 42}, addressMaps: ""}
+  const initialDirty = {name: false, surname:false , telephone:false, address:false, city:false, numberAddress:false,startHour:false}
   const [data,setData] = useState<IInitialData>(initialData)
   const [error, setError] = useState(initialData);
   const [dirty, setDirty] = useState(initialDirty);
   const [map, setMap] = useState<google.maps.Map | null>(null);
   const [marker, setMarker] = useState<google.maps.Marker | null>(null);
 
+
   useEffect(() => {
     setError({
       name: validateName(data.name),
       surname: validateName(data.surname),
       telephone: validatePhone(data.telephone),
-      address: validateAddress(data.address)
-    })
-  },[data])
-
-  useEffect(() => {
-    const loader = new Loader({
-      apiKey: `${process.env.NEXT_PUBLIC_MAPS_KEY}`,
-      version: 'weekly',
-      libraries: ['places'],
-    });
-    loader.load().then(() => {
-      initMap();
-    });
+      address: validateAddress(data.address),
+      city: validateCity(data.city),
+      numberAddress: validateNumberAddress(data.numberAddress),
+      startHour: validateStartHour(data.startHour)
   })
+    },[data])
 
-  const initMap = () => {
-    const mapOptions = {
-      center: { lat: -38.005965, lng: -57.544678 },
-      zoom: 15,
-    };
-    const mapDiv = document.getElementById('map') as HTMLElement;
-    const newMap = new google.maps.Map(mapDiv, mapOptions);
-    setMap(newMap);
-    console.log(map);
+  // useEffect(() => {
+  //   const loader = new Loader({
+  //     apiKey: `${process.env.NEXT_PUBLIC_MAPS_KEY}`,
+  //     version: 'weekly',
+  //     libraries: ['places'],
+  //   });
+  //   loader.load().then(() => {
+  //     initMap();
+  //   });
+  // })
+
+  // const initMap = () => {
+  //   const mapOptions = {
+  //     center: { lat: -38.005965, lng: -57.544678 },
+  //     zoom: 15,
+  //   };
+  //   const mapDiv = document.getElementById('map') as HTMLElement;
+  //   const newMap = new google.maps.Map(mapDiv, mapOptions);
+  //   setMap(newMap);
+  //   console.log(map);
     
 
-    const input = document.getElementById('addressMaps') as HTMLInputElement;
-    const autocomplete = new google.maps.places.Autocomplete(input, {
-      fields: ['address_components', 'geometry'],
-      types: ['address'],
-    })
+  //   const input = document.getElementById('addressMaps') as HTMLInputElement;
+  //   const autocomplete = new google.maps.places.Autocomplete(input, {
+  //     fields: ['address_components', 'geometry'],
+  //     types: ['address'],
+  //   })
 
-    autocomplete.addListener('place_changed', () => {
-      const place = autocomplete.getPlace();
-      if (place.geometry && place.geometry.location) {
-        const location = place.geometry.location;
-        const newCoordinates = { lat: location.lat(), lng: location.lng() };
-        newMap.setCenter(location);
-        if (marker) {
-          marker.setPosition(newCoordinates)
-        } else {
-          const newMarker = new google.maps.Marker({
-            position: newCoordinates,
-            map: newMap,
-          });
-          setMarker(newMarker);
-        }
-        const streetNumber = getAddressComponent(place, 'street_number') || '';
-        const route = getAddressComponent(place, 'route') || '';
-        const fullAddress = `${route} ${streetNumber} `.trim();
-        setData(prevData => ({
-          ...prevData,
-          address: fullAddress,
-          mapLocation: newCoordinates,
-        }));
-      }
-    });
-  };
+  //   autocomplete.addListener('place_changed', () => {
+  //     const place = autocomplete.getPlace();
+  //     if (place.geometry && place.geometry.location) {
+  //       const location = place.geometry.location;
+  //       const newCoordinates = { lat: location.lat(), lng: location.lng() };
+  //       newMap.setCenter(location);
+  //       if (marker) {
+  //         marker.setPosition(newCoordinates)
+  //       } else {
+  //         const newMarker = new google.maps.Marker({
+  //           position: newCoordinates,
+  //           map: newMap,
+  //         });
+  //         setMarker(newMarker);
+  //       }
+  //       const streetNumber = getAddressComponent(place, 'street_number') || '';
+  //       const route = getAddressComponent(place, 'route') || '';
+  //       const fullAddress = `${route} ${streetNumber} `.trim();
+  //       setData(prevData => ({
+  //         ...prevData,
+  //         address: fullAddress,
+  //         mapLocation: newCoordinates,
+  //       }));
+  //     }
+  //   });
+  // };
 
-  const getAddressComponent = (place: google.maps.places.PlaceResult, type: string) => {
-    const component = place.address_components?.find((comp) => comp.types.includes(type));
-    return component ? component.long_name : '';
-  };
+  // const getAddressComponent = (place: google.maps.places.PlaceResult, type: string) => {
+  //   const component = place.address_components?.find((comp) => comp.types.includes(type));
+  //   return component ? component.long_name : '';
+  // };
 
-
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {        
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {            
     setData({
       ...data, [e.target.name]: e.target.value
     });
@@ -110,6 +116,15 @@ const Form = () => {
     const res= await formService(data)
     if(res.success){
       alert("formulario subido correctamente")
+      window.location.href = `https://api.whatsapp.com/send?phone=541136021862&text=
+        // Nombre:${data.name}%20${data.surname}%20
+        // Parlantes:${data.speakers}
+        // Consola:${data.console}
+        // Microfonos:${data.microphones}
+        // Comienzo:${data.startHour}
+        // Fin:${data.endHour ? (data.endHour) : ("ninguna")}
+        // Ciudad: ${data.city}
+        // Direccion:${data.address}%20${data.numberAddress}`
     }
     else{      
       console.log(res);
@@ -188,19 +203,36 @@ const Form = () => {
         </select>
         </div>
         </div>
-          {dirty.telephone ? <p className={styles.errorText}>{error.telephone}</p> : null }
+          {/* {dirty.telephone ? <p className={styles.errorText}>{error.telephone}</p> : null } */}
         </div>
         <div className={styles.divInput}>
           <label htmlFor="">Horario</label>
+          <div className={styles.divTime}>
           <div>
-            <input type="datetime-local" name="startHour" id="startHour" />
+            <h4>Comienzo</h4>
+            <input type="datetime-local" name="startHour" id="startHour" value={data.startHour} onChange={handleChange} />
           </div>
           <div>
-            <input type="datetime-local" name="endHour" id="endHour" />
+            <h4>Fin</h4>
+            <input type="datetime-local" name="endHour" id="endHour" value={data.endHour} onChange={handleChange} />
+          </div>
           </div>
         </div>
         <div className={styles.divInput}>
+        <label htmlFor="">Ciudad</label>
+          <input 
+          type="text"
+          name="city"
+          id="city" 
+          placeholder="Ej: Merlo"
+          value={data.city}
+          onChange={handleChange}
+          />
+          {dirty.city ? <p className={styles.errorText}>{error.city}</p> : null }
+        </div>
+        <div className={styles.divInput}>
           <label htmlFor="">Direccion del evento</label>
+          <div className={styles.divAddress}>
           <input 
           type="text"
           name="address"
@@ -217,11 +249,23 @@ const Form = () => {
             onChange={handleChange}
             value={data.addressMaps}
             placeholder="Dirección"
-          className={styles.inputStyle} />
-          <div id="map" style={{ height: '400px', width: '800px' }}></div> */}
-          {dirty.address ? <p className={styles.errorText}>{error.address}</p> : null }
+            className={styles.inputStyle} />
+            <div id="map" style={{ height: '400px', width: '800px' }}></div> */}
+          <label htmlFor="">N°</label>
+          <input 
+          type="text"
+          name="numberAddress"
+          id="numberAddress" 
+          placeholder="134"
+          value={data.numberAddress}
+          onChange={handleChange}
+          />
         </div>
-        {error.address || error.name || error.address || error.telephone ? (
+        {dirty.address ? <p className={styles.errorText}>{error.address}</p> : null }
+        {dirty.numberAddress ? <p className={styles.errorText}>{error.numberAddress}</p> : null }
+
+        </div>
+        {error.address || error.name || error.address || error.telephone || error.numberAddress || error.city ||error.startHour ? (
         <div className={styles.divButtonError}>
         <button disabled={true}>Averiguar Presupuesto</button>
         </div>
--- front/src/helpers/validation.tsx
+++ front/src/helpers/validation.tsx
@@ -13,13 +13,34 @@ export const validatePhone = (name:string): string => {
     if(!regexCountry.test(name)) validation = " Ingresa Sólo números entre 9 y 15 dígitos"
     return validation
 }
+export const validateNumberAddress = (name:string): string => {
+    let validation = ""
+    const regexCountry = /^\d{1,7}$/
+
+    if(!regexCountry.test(name)) validation = " Ingresa Sólo numeros"
+    return validation
+}
+
+export const validateStartHour = (hour:string):string => {
+    let validation = ""
+    if(!hour) validation = "Ingresa una hora de comienzo"
+    return validation
+}
+
+export const validateCity = (city:string):string => {
+    let validation = ""
+    const regexAddress = /^[a-zA-Z0-9\s,.'-]{3,}$/;
+    if(!regexAddress.test(city)) validation = "Ingresa la ciudad correctamente"
+
+    return validation
+}
 
 export const validateAddress = (address: string): string => {
     let validation = "";
-    const regexAddress = /^[a-zA-Z0-9\s,.'-]{5,}$/;
+    const regexAddress = /^[a-zA-Z0-9\s,.'-]{3,}$/;
 
     if (!regexAddress.test(address)) {
-        validation = "Formato de dirección no válido. Debe tener al menos 5 caracteres e incluir letras y números.";
+        validation = "Formato de dirección no válido. Debe tener al menos 3 letras";
     }
 
     return validation;
